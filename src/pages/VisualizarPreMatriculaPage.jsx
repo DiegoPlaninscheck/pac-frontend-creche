fc4167b4d1b5a70246025866f3185938a9c80971
Diego <92130153+DiegoPlaninscheck@users.noreply.github.com>
2025-09-17 19:53:39 -0300

    Update VisualizarPreMatriculaPage.jsx

--- src/pages/VisualizarPreMatriculaPage.jsx
+++ src/pages/VisualizarPreMatriculaPage.jsx
@@ -8,14 +8,14 @@ const VisualizarPreMatricula = () => {
 
     const [data, setData] = useState([]);
 
-    useEffect(() => {
-        axios.get("/registration/registrationAddress").then((res) => {
-            for (let item of res.data) {
-                item.dataNascimento = item.dataNascimento.split("T")[0]
-            }
-            setData(res.data)
-        });
-    }, []);
+    // useEffect(() => {
+    //     axios.get("/registration/registrationAddress").then((res) => {
+    //         for (let item of res.data) {
+    //             item.dataNascimento = item.dataNascimento.split("T")[0]
+    //         }
+    //         setData(res.data)
+    //     });
+    // }, []);
 
     // function exportToExcel() {
     //     console.log('Exporting to Excel');
@@ -30,7 +30,7 @@ const VisualizarPreMatricula = () => {
         <>
             <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', margin: '0 1rem 0 1rem' }}>
                 <Typography sx={{ fontFamily: "Montserrat" }} variant="h4" component="h1" gutterBottom>Visualizar Pré-Matrícula</Typography>
-                <Button variant="contained" color="primary" onClick={() => exportToExcel()}>Export to Excel</Button>
+                <Button variant="contained" color="primary" onClick={() => {}}>Export to Excel</Button>
             </Box >
             <TablePreMatricula data={data} />
         </>
